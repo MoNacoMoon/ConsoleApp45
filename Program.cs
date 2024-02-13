798c0aa647f353ec1419b4d2037ad536e9b3acfc
evol_ <evol_@HP>
2024-02-13 20:17:07 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] omas = new int[6];
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+
+            //int i;
+            //int[] omas = new int[6];
+            //for (i = 0; i < omas.Length; i++)
+            //{
+            // Console.Write($"omas[{i}]=");
+            // omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (i = 0; i < omas.Length; i++)
+            //{
+            // Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}   
+
+
+            //int i;
+            //int[] omas = { -3, 5, 7, -1, 10, 15 };
+            //for (i = 0; i < omas.Length; i++)
+            //{
+            // Console.Write($"omas[{i}]={omas[i]}");
+            //}
+
+
+
+            //int i;
+            //int[] omas = new int[6];
+            //for (i = 0; i < 6; i++)
+            //{
+            //    omas[i] = i * i - 1;
+            //}
+            //foreach (int elem in omas) Console.WriteLine(elem);
+
+
+
+            //Random r = new Random();
+            //int[] omas = new int[6];
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    omas[i] = r.Next(1, 101);
+            //}
+            //foreach (int elem in omas)
+            //{
+            //    Console.WriteLine(elem);
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
+        
+    
+
